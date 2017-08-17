397cdb02794188816c97585a442e00a0e661f204
dmitry <d.semyrjazhko@gmail.com>
2017-08-17 22:26:12 +0300

    added stack solution

--- StringPermutations/src/rambler/algorithms/Main.java
+++ StringPermutations/src/rambler/algorithms/Main.java
@@ -1,22 +1,45 @@
 package rambler.algorithms;
 
+import java.util.Stack;
+
 public class Main {
     public static void main(String args[]) {
-        permuteString("", "ABC");
-    }
+        String input = "ABCD";
 
-    public static void permuteString(String beginningString, String endingString) {
-        if (endingString.length() <= 1)
-            System.out.println(beginningString + endingString);
-        else
-            for (int i = 0; i < endingString.length(); i++) {
-                try {
-                    String newString = endingString.substring(0, i) + endingString.substring(i + 1);
+        Stack<String> stack = new Stack<>();
+        stack.push("");
+        stack.push(input);
 
-                    permuteString(beginningString + endingString.charAt(i), newString);
-                } catch (StringIndexOutOfBoundsException exception) {
-                    exception.printStackTrace();
+        while (!stack.empty()) {
+            String rest = stack.pop();
+            String base = stack.pop();
+
+            for (int i = 0; i < rest.length(); i++) {
+                String currentBase = base + rest.charAt(i);
+                String currentRest = rest.substring(0, i) + rest.substring(i + 1);
+
+                if (currentRest.length() > 1) {
+                    stack.push(currentBase);
+                    stack.push(currentRest);
                 }
+                else
+                    System.out.println(currentBase + currentRest);
             }
+        }
     }
+
+//    public static void permuteString(String beginningString, String endingString) {
+//        if (endingString.length() <= 1)
+//            System.out.println(beginningString + endingString);
+//        else
+//            for (int i = 0; i < endingString.length(); i++) {
+//                try {
+//                    String newString = endingString.substring(0, i) + endingString.substring(i + 1);
+//
+//                    permuteString(beginningString + endingString.charAt(i), newString);
+//                } catch (StringIndexOutOfBoundsException exception) {
+//                    exception.printStackTrace();
+//                }
+//            }
+//    }
 }
