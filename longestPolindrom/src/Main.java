f8a15301a375fdf15a7a8c8cc5d7ff4bccb3a1be
dmitry <d.semyrjazhko@gmail.com>
2017-08-22 23:18:23 +0300

    small refactor

--- longestPolindrom/src/Main.java
+++ longestPolindrom/src/Main.java
@@ -2,20 +2,18 @@ public class Main {
     public static void main(String[] args) {
         String input = "bacaba";
 
-        char[] arr = input.toCharArray();
-
         int left = -1;
         int max = 0;
         int counter = 0;
         String strMax = null;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (left >= 0 && arr[left - (i - left)] == arr[i]) {
+        for (int i = 0; i < input.length(); i++) {
+            if (isInsidePolindrom(input, left, i)) {
                 counter++;
             }
 
             if(left >= 0) {
-                if (arr[left - (i - left)] != arr[i]) {
+                if (isOutsidePolindrom(input, left, i)) {
                     if (counter > max) {
                         max = counter;
                         strMax = input.substring(left - (i - left)+1, i);
@@ -24,7 +22,7 @@ public class Main {
                     left=-1;
                     counter=0;
                 }
-                if (i == arr.length-1) {
+                if (isLastCharacter(input, i)) {
                     if (counter > max) {
                         max = counter;
                         strMax = input.substring(left - (i - left), i+1);
@@ -35,13 +33,29 @@ public class Main {
                 }
             }
 
-            if (i + 2 < arr.length && arr[i] == arr[i + 2]) {
+            if (isCenterOfPolindrom(input, i)) {
                 left = i + 1;
                 i++;
             }
         }
 
         System.out.println("Result: " + strMax);
-        System.out.println("Max: " + max);
+        System.out.println("Max length: " + max);
+    }
+
+    private static boolean isCenterOfPolindrom(String input, int i) {
+        return i + 2 < input.length() && input.charAt(i) == input.charAt(i + 2);
+    }
+
+    private static boolean isLastCharacter(String input, int i) {
+        return i == input.length()-1;
+    }
+
+    private static boolean isOutsidePolindrom(String input, int left, int i) {
+        return input.charAt(left - (i - left)) != input.charAt(i);
+    }
+
+    private static boolean isInsidePolindrom(String input, int left, int i) {
+        return left >= 0 && input.charAt(left - (i - left)) == input.charAt(i);
     }
 }
