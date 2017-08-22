7f91ca7b6cbabc98cbbacf0c502d868c79a64a35
d.semiryazhko <d.semiryazhko@rambler-co.ru>
2017-08-22 17:25:15 +0300

    LongestPolindrom

---
+++ LongestPolindrom/src/rambler/algorithms/LongestPolindrom/Main.java
@@ -0,0 +1,32 @@
+package rambler.algorithms.LongestPolindrom;
+
+public class Main {
+    public static void main(String[] args) {
+        String input = "dcbanabc";
+
+        char[] arr = input.toCharArray();
+
+        int left = -1;
+        int max = 0;
+        int counter = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (left >= 0 && arr[left - (left - i)] == arr[i]) {
+                counter++;
+            } else {
+                left = -1;
+            }
+
+            if (i + 2 < arr.length && arr[i] == arr[i + 2]) {
+                left = i + 1;
+                i++;
+            }
+
+            if (counter > max) {
+                max = counter;
+            }
+        }
+
+        System.out.println("Max: " + max);
+    }
+}
