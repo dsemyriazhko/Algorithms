875cbfe87c99785df385e5e3b34be47a51b9c7c1
dmitry <d.semyrjazhko@gmail.com>
2017-09-11 21:36:43 +0300

    Дано: числа `a` и `b` `a` – число симоволов `0` в строке, `b` - число символов `1`. В строке не может быть двух подряд символов `0`
    Вопрос:
    Сколько существует таких строк?
    
    Обязательное условие:
    решение должно быть рекурсивным

---
+++ zeroOnePermutations/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.HashSet;
+
+public class Main {
+    private static HashSet<String> cache = new HashSet<>();
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 1;
+
+        StringBuilder input = new StringBuilder(a+b);
+        for (int i=0; i<a; ++i) {
+            input.append('0');
+        }
+        for (int i=0; i<b; ++i) {
+            input.append('1');
+        }
+
+        permuteString("", input.toString());
+    }
+
+    private static void permuteString(String beginningString, String endingString) {
+        if (endingString.length() <= 1) {
+            String result = beginningString + endingString;
+            if (!cache.contains(result) && isEndingAnotherChar(beginningString, endingString.charAt(0))) {
+                System.out.println(result);
+                cache.add(result);
+            }
+        }
+        else {
+            for (int i = 0; i < endingString.length(); i++) {
+                try {
+                    String newString = endingString.substring(0, i) + endingString.substring(i + 1);
+
+                    if (isEndingAnotherChar(beginningString, endingString.charAt(i)))
+                        permuteString(beginningString + endingString.charAt(i), newString);
+                } catch (StringIndexOutOfBoundsException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static boolean isEndingAnotherChar(String baseString, char value) {
+        return "".equals(baseString) || baseString.charAt(baseString.length()-1) != value;
+    }
+}
