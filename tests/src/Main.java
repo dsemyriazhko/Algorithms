d33aa49dea5e6023f4ee96abe35d8830d1e9bf51
dmitry <d.semyrjazhko@gmail.com>
2017-10-09 20:10:21 +0300

    common tests

---
+++ tests/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by dmitry on 09.10.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        double i = 0.0 / 0.0;
+
+        System.out.println(i - i == 0);
+    }
+}
